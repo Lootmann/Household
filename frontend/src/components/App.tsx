e2641c331258c528852e01d2a3d4958d0f2083c3
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-03-06 04:33:56 +0900

    frontend: App.tsx add InputForm

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import Header from "./Header";
+import InputForm from "./InputForm";
 
 function App() {
   return (
@@ -7,11 +7,13 @@ function App() {
       <Header />
 
       <div className="h-[calc(100vh-4rem)] flex flex-col gap-2 p-4 text-xl">
-        <div className="flex justify-between gap-2 flex-grow h-1/2 border-2 border-slate-300 p-2">
+        <div className="h-1/2 flex-grow flex justify-between gap-2 border-2 border-slate-300 p-2">
           <div className="flex-1 border-2 border-slate-700 p-2">This Month</div>
           <div className="flex-1 border-2 border-slate-700 p-2">This Week</div>
           <div className="flex-1 border-2 border-slate-700 p-2">This Day</div>
-          <div className="flex-1 border-2 border-slate-700 p-2">Input Form</div>
+          <div className="flex-1 border-2 border-slate-700 p-2">
+            <InputForm />
+          </div>
         </div>
 
         <div className="h-1/2 flex-grow border-2 border-slate-300 p-4">
