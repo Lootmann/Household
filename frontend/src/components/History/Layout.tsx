ed7c771b4ad212e1c9de022a7ccde3977ae5af44
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-03-06 08:53:32 +0900

    frontend: History/layout.tsx show table histories

--- frontend/src/components/History/Layout.tsx
+++ frontend/src/components/History/Layout.tsx
@@ -13,30 +13,39 @@ function History() {
   }, []);
 
   return (
-    <div className="h-full gap-2">
-      <table className="table-auto">
-        <thead>
-          <tr>
-            <th>Registered At</th>
-            <th>Amout</th>
-            <th>Category</th>
-            <th>Memo</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {histories.map((history) => (
-            <tr key={history.id}>
-              <td>{history.registered_at}</td>
-              <td>{history.amount}</td>
-
-              {/* TODO: show category name instead category_id */}
-              <td>{history.category}</td>
-              <td>{history.memo}</td>
+    <div className="h-full gap-2 flex">
+      <div className="flex flex-col gap-2 flex-1 p-2 border-2 border-slate-400 bg-slate-400 rounded-md">
+        <table className="table-fixed w-full bg-slate-300 border-separate rounded-md">
+          <thead>
+            <tr>
+              <th className="text-left px-4">Registered</th>
+              <th className="text-left px-2">Amout</th>
+              <th className="text-left px-2">Category</th>
+              <th className="text-left px-2">Memo</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+
+          <tbody>
+            {histories.map((history) => (
+              <tr key={history.id}>
+                <td className="px-4 ">{history.registered_at}</td>
+                <td className="px-2">{history.amount}</td>
+                <td className="px-2">{history.category.name}</td>
+                <td className="px-2">{history.memo}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        {/* TODO: pagination */}
+        <div className="flex flex-col items-center">
+          <p>&lt;First &lt; Prev [Pagination] Next &gt; Last &gt;</p>
+        </div>
+      </div>
+
+      <div className="flex-1 p-2 text-2xl border-2 border-slate-400 rounded-md">
+        <h2>Left Something</h2>
+      </div>
     </div>
   );
 }
