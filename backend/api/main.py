1e053a0d9396f639a24c0971acaacbdb728d8c38
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-03-05 13:44:34 +0900

    backend: router fix category router name, impl households router

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,7 +1,7 @@
+from api.routers.categories import router as category_router
+from api.routers.households import router as household_router
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-from api.routers.categories import router as user_router
 
 app = FastAPI()
 
@@ -17,4 +17,5 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(user_router)
+app.include_router(category_router)
+app.include_router(household_router)
