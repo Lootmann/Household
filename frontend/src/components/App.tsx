ebef7d423723c9c1a89019a40b17fec66075b6f5
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-03-06 02:57:49 +0900

    frontend: top page layout

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -6,9 +6,25 @@ function App() {
     <div className="h-screen bg-slate-500">
       <Header />
 
-      <div className="h-[calc(100vh-4rem)] flex flex-col p-4 text-xl">
-        <div className="flex-grow bg-yellow-900 p-4">hello world</div>
-        <div className="flex-grow bg-sky-800 p-4">hello world</div>
+      <div className="h-[calc(100vh-4rem)] flex flex-col gap-2 p-4 text-xl">
+        <div className="flex justify-around gap-2 flex-grow border-2 border-slate-300 p-2">
+          <div className="flex-grow border-2 border-slate-700 p-2">
+            This Month
+          </div>
+          <div className="flex-grow border-2 border-slate-700 p-2">
+            This Week
+          </div>
+          <div className="flex-grow border-2 border-slate-700 p-2">
+            This Day
+          </div>
+          <div className="flex-grow border-2 border-slate-700 p-2">
+            Input Form
+          </div>
+        </div>
+
+        <div className="flex-grow border-2 border-slate-300 p-4">
+          Detailed Analysis - 何を表示しようかな ...
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,8 +2,13 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="bg-slate-700 p-4">
+    <div className="flex gap-4 items-baseline p-4 bg-slate-700 text-slate-200 ">
       <h1 className="text-2xl text-slate-200">Header</h1>
+      <p>ヘッダーいるか？</p>
+      <p>むしろ無いほうがスッキリしている気もする</p>
+      <p>
+        というより位置ページに収まるように全部表示できたほうがより使いやすくね
+      </p>
     </div>
   );
 }
