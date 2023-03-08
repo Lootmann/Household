cc6fc23589647981864d41b4068809f778de15bc
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-03-08 11:27:48 +0900

    frontend: fix nivo error - update all packages (delete, and re-intsall)

--- frontend/src/components/Aggregates/ThisMonth.tsx
+++ frontend/src/components/Aggregates/ThisMonth.tsx
@@ -4,7 +4,7 @@ import { ResponsiveBar } from "@nivo/bar";
 
 function ThisMonth({ households }: { households: APIHouseholdType[] }) {
   const [data, setData] = React.useState<any>([]);
-  const [keys, setKeys] = React.useState<string[]>();
+  const [keys, setKeys] = React.useState<string[]>([]);
 
   React.useEffect(() => {
     setData(
@@ -22,36 +22,37 @@ function ThisMonth({ households }: { households: APIHouseholdType[] }) {
 
   return (
     <>
-      {households.length !== 0 && (
-        <div className="h-full">
-          <h2 className="text-2xl">Show This Month Expenses Chart</h2>
-          <ResponsiveBar
-            data={data}
-            keys={keys}
-            theme={{
-              axis: {
-                ticks: {
-                  text: {
-                    fontSize: 20,
-                  },
+      <div className="h-full">
+        <h2 className="text-2xl">Show This Month Expenses Chart</h2>
+        <ResponsiveBar
+          data={data}
+          keys={keys}
+          theme={{
+            fontSize: 20,
+            axis: {
+              ticks: {
+                text: {
+                  fontSize: 20,
                 },
               },
-            }}
-            legends={[
-              {
-                dataFrom: "keys",
-                anchor: "right",
-                direction: "column",
-                itemWidth: 10,
-                itemHeight: 25,
-              },
-            ]}
-            margin={{ top: 50, right: 70, bottom: 50, left: 50 }}
-            padding={0.4}
-            layout="horizontal"
-          />
-        </div>
-      )}
+            },
+          }}
+          legends={[
+            {
+              dataFrom: "keys",
+              anchor: "right",
+              direction: "column",
+              itemWidth: 30,
+              itemHeight: 30,
+            },
+          ]}
+          margin={{ top: 50, right: 70, bottom: 50, left: 70 }}
+          padding={0.5}
+          innerPadding={2}
+          groupMode="stacked"
+          layout="vertical"
+        />
+      </div>
     </>
   );
 }
