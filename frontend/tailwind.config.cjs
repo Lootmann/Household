b7c29fd656b555113eb652fbba519ebc12a1d4f2
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-03-09 03:41:46 +0900

    tsconfig, tailwindcss: flex settings

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      flex: {
+        2: "2 1 0",
+      },
+    },
   },
   plugins: [],
 };
