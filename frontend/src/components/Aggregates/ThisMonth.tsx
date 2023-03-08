0ac200852ddd98152f974a781d0211df73ee6f81
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-03-08 12:46:09 +0900

    frontend: Aggregates/ThisMonth - show this month's Expences with Nivo

--- frontend/src/components/Aggregates/ThisMonth.tsx
+++ frontend/src/components/Aggregates/ThisMonth.tsx
@@ -23,35 +23,79 @@ function ThisMonth({ households }: { households: APIHouseholdType[] }) {
   return (
     <>
       <div className="h-full">
-        <h2 className="text-2xl">Show This Month Expenses Chart</h2>
-        <ResponsiveBar
-          data={data}
-          keys={keys}
-          theme={{
-            fontSize: 20,
-            axis: {
-              ticks: {
-                text: {
-                  fontSize: 20,
+        <h2 className="text-2xl text-center">This Month Expenses Chart</h2>
+
+        {data.length > 0 && keys.length > 0 && (
+          <ResponsiveBar
+            data={data}
+            keys={keys}
+            // theme
+            theme={{
+              fontSize: 20,
+              axis: {
+                ticks: {
+                  text: {
+                    fontSize: 16,
+                  },
                 },
               },
-            },
-          }}
-          legends={[
-            {
-              dataFrom: "keys",
-              anchor: "right",
-              direction: "column",
-              itemWidth: 30,
-              itemHeight: 30,
-            },
-          ]}
-          margin={{ top: 50, right: 70, bottom: 50, left: 70 }}
-          padding={0.5}
-          innerPadding={2}
-          groupMode="stacked"
-          layout="vertical"
-        />
+              grid: {
+                line: {
+                  stroke: "#475577",
+                  strokeWidth: 1,
+                },
+              },
+              legends: {
+                text: {
+                  fontSize: 21,
+                  fill: "#000000",
+                },
+              },
+            }}
+            // legends
+            legends={[
+              {
+                dataFrom: "keys",
+                anchor: "right",
+                direction: "column",
+                itemWidth: 30,
+                itemHeight: 50,
+              },
+            ]}
+            margin={{ top: 30, right: 90, bottom: 60, left: 80 }}
+            padding={0.5}
+            innerPadding={2}
+            groupMode="stacked"
+            layout="vertical"
+            minValue={0}
+            maxValue={30000}
+            valueScale={{ type: "linear" }}
+            // border
+            borderRadius={5}
+            borderWidth={1}
+            // borderColor={{
+            //   from: "color",
+            //   modifiers: [["darker", 0.0]],
+            // }}
+            // grid
+            enableGridX={false}
+            enableGridY={true}
+            // axis
+            axisTop={null}
+            axisRight={null}
+            axisBottom={null}
+            axisLeft={{
+              tickSize: 10,
+              tickPadding: 0,
+              legendOffset: 20,
+            }}
+            // label
+            labelTextColor={{
+              from: "color",
+              modifiers: [["darker", 5.0]],
+            }}
+          />
+        )}
       </div>
     </>
   );
